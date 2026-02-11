72e75cc92a4192949a2121d8fafc078121671432
Murtaza Rahimi <murtaza2003rahimi@gmail.com>
2026-02-11 10:00:33 +0430

    Update API route to support pagination, currency and search

--- src/app/api/crypto/route.ts
+++ src/app/api/crypto/route.ts
@@ -1,21 +1,31 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+
+  const page = searchParams.get("page") || "1";
+  const currency = searchParams.get("currency") || "usd";
+  const search = searchParams.get("search");
+
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_COINGECKO_API}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`,
-      { cache: "no-store" },
-    );
+    let url = "";
+
+    if (search) {
+      url = `${process.env.NEXT_PUBLIC_COINGECKO_API}/search?query=${search}`;
+    } else {
+      url = `${process.env.NEXT_PUBLIC_COINGECKO_API}/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=12&page=${page}&sparkline=false`;
+    }
+
+    const res = await fetch(url, { cache: "no-store" });
 
     if (!res.ok) {
-      throw new Error("Failed to fetch crypto data");
+      throw new Error("Failed to fetch data");
     }
 
     const data = await res.json();
 
     return NextResponse.json(data);
   } catch (error) {
-    console.error(error);
     return NextResponse.json(
       { error: "Something went wrong" },
       { status: 500 },
